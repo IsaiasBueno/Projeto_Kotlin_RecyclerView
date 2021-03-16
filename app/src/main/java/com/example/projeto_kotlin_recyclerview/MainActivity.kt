5ad60072a0f5d0e76619f19acd4dd7fc4db74947
IsaiasBueno <izaias.bueno@gmail.com>
2021-03-16 20:28:17 -0300

    Projeto Recycler View - Lista de E-mail

---
+++ app/src/main/java/com/example/projeto_kotlin_recyclerview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projeto_kotlin_recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
