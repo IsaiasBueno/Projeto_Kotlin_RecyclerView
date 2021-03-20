cc58b2b0ace18d710cd174c9f67c4bf5d46313a5
IsaiasBueno <izaias.bueno@gmail.com>
2021-03-20 16:49:17 -0300

    Inclusão de Adapter.toggleSelection na MainActivity.kt e da fun toggleSelection no EmailAdapter.kt,inclusão do "Menu Delete" e do Icone "ic_baseline_delete...",inclusão do "windowActionModeOverlay" na styles.xml

--- app/src/main/java/com/example/projeto_kotlin_recyclerview/EmailAdapter.kt
+++ app/src/main/java/com/example/projeto_kotlin_recyclerview/EmailAdapter.kt
@@ -1,20 +1,34 @@
 package com.example.projeto_kotlin_recyclerview
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.graphics.Color
 import android.graphics.Color.rgb
 import android.graphics.Typeface
 import android.graphics.Typeface.BOLD
 import android.graphics.Typeface.NORMAL
+import android.graphics.drawable.GradientDrawable
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.OvalShape
+import android.util.SparseBooleanArray
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.DecelerateInterpolator
+import android.widget.TextView
+import androidx.constraintlayout.solver.widgets.Rectangle
+import androidx.core.util.isNotEmpty
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projeto_kotlin_recyclerview.model.Email
 import kotlinx.android.synthetic.main.email_item.view.*
+import java.text.FieldPosition
 
 class EmailAdapter (val emails : MutableList<Email>): RecyclerView.Adapter<EmailAdapter.EmailViewHolder>() {
+
+    val selectedItems = SparseBooleanArray()
+    private var currentSelectedPos: Int = -1
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmailViewHolder {
      val view = LayoutInflater.from(parent.context).inflate(R.layout.email_item, parent,false)
@@ -24,7 +38,39 @@ class EmailAdapter (val emails : MutableList<Email>): RecyclerView.Adapter<Email
 
     override fun onBindViewHolder(holder: EmailViewHolder, position: Int) {
         holder.bind(emails[position])
+        holder.itemView.setOnClickListener {
+            if (selectedItems.isNotEmpty())
+            onItemClick?.invoke(position)
+        }
+        holder.itemView.setOnLongClickListener {
+            onItemLongClick?.invoke(position)
+            return@setOnLongClickListener true
+        }
+        if (currentSelectedPos == position) currentSelectedPos = -1
     }
+
+    fun toggleSelection(position: Int){
+        currentSelectedPos = position
+        if (selectedItems[position,false]) {
+            selectedItems.delete(position)
+            emails[position].selected = false
+        }else{
+            selectedItems.put(position,true)
+            emails[position].selected = true
+        }
+        notifyItemChanged(position)
+    }
+
+    fun deleteEmails() {
+        emails.removeAll(
+            emails.filter {it.selected}
+        )
+        notifyDataSetChanged()
+        currentSelectedPos = -1
+    }
+
+    var onItemClick: ((Int) -> Unit)? = null
+    var onItemLongClick: ((Int) -> Unit)? = null
 
     inner class EmailViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView) {
         fun bind(email: Email) {
@@ -46,11 +92,43 @@ class EmailAdapter (val emails : MutableList<Email>): RecyclerView.Adapter<Email
                 else R.drawable.ic_baseline_star_border_24
                 )
             }
-
+            if(email.selected){
+                itemView.txt_icon.background = itemView.txt_icon.oval(
+                    Color.rgb(26,115,233)
+                )
+                itemView.background = GradientDrawable().apply {
+                    shape = GradientDrawable.RECTANGLE
+                    cornerRadius = 32f
+                    setColor(Color.rgb(232,240,253))
+                }
+            }else{
+                itemView.background = GradientDrawable().apply {
+                    shape = GradientDrawable.RECTANGLE
+                    cornerRadius = 32f
+                    setColor(Color.WHITE)
+            }
         }
-
+            if(selectedItems.isNotEmpty())
+                animate(itemView.txt_icon, email)
     }
-
+        private fun animate(view:TextView, email: Email){
+            val oa1 = ObjectAnimator.ofFloat(view,"scaleX",1f,0f)
+            val oa2 = ObjectAnimator.ofFloat(view,"scaleX",0f,1f)
+            oa1.interpolator = DecelerateInterpolator()
+            oa2.interpolator = AccelerateDecelerateInterpolator()
+            oa1.duration = 200
+            oa2.duration = 200
+            oa1.addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    super.onAnimationEnd(animation)
+                    if(email.selected)
+                        view.text = "\u2713"
+                    oa2.start()
+                }
+            })
+            oa1.start()
+        }
+    }
 }
 
 fun View.oval(@colorInt color:Int) : ShapeDrawable{
--- app/src/main/java/com/example/projeto_kotlin_recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/projeto_kotlin_recyclerview/MainActivity.kt
@@ -2,18 +2,28 @@ package com.example.projeto_kotlin_recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.ActionMode
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchUIUtil
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.projeto_kotlin_recyclerview.model.email
 import com.example.projeto_kotlin_recyclerview.model.fakeEmails
 import com.mooveit.library.Fakeit.Companion.fakeit
 import kotlinx.android.synthetic.main.activity_main.*
 
 import com.mooveit.library.Fakeit;
+import java.text.FieldPosition
 import java.text.SimpleDateFormat
 import java.util.*
 
+
 class MainActivity : AppCompatActivity() {
     private lateinit var adapter: EmailAdapter
+    private var actionMode: androidx.appcompat.view.ActionMode? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +36,86 @@ class MainActivity : AppCompatActivity() {
 
         fab.setOnClickListener {
             addEmail()
-            recycler_view_main.scrollToPosition(0)
+          //  recycler_view_main.scrollToPosition(0)
+        }
+        val helper =
+            androidx.recyclerview.widget.ItemTouchHelper(
+                ItemTouchHelper(0,
+                androidx.recyclerview.widget.ItemTouchHelper.LEFT)
+            )
+
+        helper.attachToRecyclerView(recycler_view_main)
+        adapter.onItemClick = {
+            enableActionMode(it)
+        }
+        adapter.onItemLongClick = {
+            enableActionMode(it)
+        }
+    }
+
+    private fun enableActionMode(position: Int) {
+        if (ActionMode == null)
+            actionMode = startSupportActionMode(object: ActionMode.Callback {
+
+                override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
+                    if(item?.itemId == R.id.action_delete) {
+                        adapter.deleteEmails()
+                        mode?.finish()
+                        return true
+                    }
+                    return false
+                }
+
+                override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+                    mode?.menuInflater?.inflate(R.menu.menu_delete,menu)
+                    return true
+                }
+
+                override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+                    return false
+                }
+
+                override fun onDestroyActionMode(mode: ActionMode?) {
+                    adapter.selectedItems.clear()
+                    adapter.emails
+                        .filter {it.selected}
+                        .forEach {it.selected = false}
+
+                    adapter.notifyDataSetChanged()
+                    actionMode = null
+                }
+            })
+
+        adapter.toggleSelection(position)
+        val size:Int = adapter.selectedItems.size()
+        if (size == 0) {
+            actionMode?.finish()
+        }else{
+            actionMode?.title = "$size"
+            actionMode?.invalidate()
+        }
+    }
+
+    inner class ItemTouchHelper(dragDirs:Int,swipeDirs:Int) : androidx.recyclerview.widget.ItemTouchHelper.SimpleCallback(
+        dragDirs,swipeDirs,
+    ) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            val from = viewHolder.adapterPosition
+            val to = target.adapterPosition
+
+            Collections.swap(adapter.emails,from, to)
+            adapter.notifyItemMoved(from,to)
+
+            return true
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            adapter.emails.removeAt(viewHolder.adapterPosition)
+            adapter.notifyItemRemoved(viewHolder.adapterPosition)
         }
     }
 
